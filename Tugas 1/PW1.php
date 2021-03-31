722af240a445ec41e7ede9fb5628e3be289e3b2d
KrisnaMurti-21 <krisnamr27@gmail.com>
2021-04-01 05:23:52 +0700

    pertama

---
+++ Tugas 1/PW1.php
@@ -0,0 +1,9 @@
+<html>
+    <head>Contoh</head>
+    <body>
+        <h1>Heading 1</h1>
+        <?php
+            echo"<P>Hello World</p>"
+        ?>
+    </body>
+</html>
---
+++ Tugas 1/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<h1> Nama : Krisna Murti <h1><br>";
+
+echo "<br>";
+
+echo "<h1> Nim : K3519049 <h1><br>";
+?>
